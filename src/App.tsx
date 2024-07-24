57871e5642783c1c3411d6f070eb09cbac7901e1
Mikakokeshi <mikakokeshi00124@gmail.com>
2024-07-24 18:23:06 +0900

    Hope画面修正/レイアウトのthemeインポート

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
-import React from 'react';
-import './App.css';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Report from './pages/Report';
-import Nomatch from './pages/Nomatch';
-import AppLayout from './components/layout/AppLayout';
+import React from "react";
+import "./App.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Report from "./pages/Report";
+import Nomatch from "./pages/Nomatch";
+import AppLayout from "./components/layout/AppLayout";
+import { theme } from "./theme/theme";
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
 
 function App() {
   return (
-   <Router>
-    <Routes>
-      <Route path="/" element={<AppLayout />}>
-        <Route path="/" element={ <Home /> } />
-        <Route path="/report" element={ <Report /> } />
-        <Route path="*" element={ <Nomatch /> } />
-      </Route>
-    </Routes>
-   </Router>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Routes>
+          <Route path="/" element={<AppLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/report" element={<Report />} />
+            <Route path="*" element={<Nomatch />} />
+          </Route>
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Box } from "@mui/material";
+import React from "react";
+import MonthlySummary from "../components/MonthlySummary";
+import Calendar from "../components/Calendar";
+import TransactionMenu from "../components/TransactionMenu";
+import TransactionForm from "../components/TransactionForm";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <Box sx={{ display: "flex" }}>
+      <Box
+        sx={{
+          flexGlow: 1,
+          // bgcolor: "#fff",
+          // boxShadow: 3,
+          // padding: 2,
+          // borderRadius: "10px",
+        }}
+      >
+        <MonthlySummary />
+        <Calendar />
+      </Box>
+      <Box>
+        <TransactionMenu />
+        <TransactionForm />
+      </Box>
+    </Box>
+  );
+};
 
-export default Home
+export default Home;
