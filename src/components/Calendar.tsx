bd54840b4891af3b703f332560e0e1176f8f2122
Mikakokeshi <mikakokeshi00124@gmail.com>
2024-07-25 18:25:47 +0900

    Fullcalendar設定/eventContent追加しカスタムで収支バランスを表示

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
-
+import FullCalendar from "@fullcalendar/react";
+import React from "react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import jaLocale from "@fullcalendar/core/locales/ja";
+import { EventContentArg } from "@fullcalendar/core";
+import "../calendar.css";
 const Calendar = () => {
-  return (
-    <div>Calendar</div>
-  )
-}
+  const events = [
+    { title: "Meeting", start: new Date() },
+    {
+      title: "aaaa",
+      start: "2024-07-25",
+      income: 2000,
+      expense: 300,
+      balance: 1700,
+    },
+  ];
 
-export default Calendar
+  const renderEventContent = (eventInfo: EventContentArg) => {
+    console.log(eventInfo);
+    return (
+      <div>
+        <div className="money" id="event-income">
+          {eventInfo.event.extendedProps.income}
+        </div>
+        <div className="money" id="event-expense">
+          {eventInfo.event.extendedProps.expense}
+        </div>
+        <div className="money" id="event-balance">
+          {eventInfo.event.extendedProps.balance}
+        </div>
+      </div>
+    );
+  };
+  return (
+    <FullCalendar
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
+      locale={jaLocale}
+      events={events}
+      eventContent={renderEventContent}
+    />
+  );
+};
+
+export default Calendar;
